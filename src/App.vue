9f7e3a006dfc9b3dd5580a2fc86311dfaefeb012
wormkung99 <wormkung99@gmail.com>
2023-05-19 20:28:14 +0700

    portfolio-app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <navbar />
+    <div>
+      <hero />
+    </div>
+    <div>
+      <about />
+      <portfolio />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import hero from "./components/hero.vue";
+import navbar from "./components/navbar.vue";
+import about from "./components/about.vue";
+import portfolio from "./components/portfolio.vue";
+
+</script>
+
+<style scoped></style>
---
+++ src/components/about.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="about">
+    <section class="bg-white dark:bg-gray-900">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+          <h2
+            class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
+          >
+            About
+          </h2>
+        </div>
+        <div class="grid gap-8 lg:grid-cols-2">
+          <article
+            class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          >
+            <h2
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              Education
+            </h2>
+            <p class="mb-5 text-gray-600 dark:text-gray-400">
+              2020 <br />Intership <br />
+              Cat telecom suratthani
+            </p>
+            <hr />
+            <br />
+            <p class="mb-5 text-gray-500 dark:text-gray-400">
+              2020 <br />Bachelor of science <br />
+              Information technology ( IT )
+            </p>
+            <hr />
+            <br />
+            <p class="mb-5 text-gray-500 dark:text-gray-400">
+              2017 <br />High school diploma <br />
+              Donsak phadungwit
+            </p>
+          </article>
+
+          <article
+            class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          >
+            <h2
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              Skill
+            </h2>
+            <div>
+              <div class="mb-1 text-base font-medium dark:text-white">
+                Html Css Javascript
+              </div>
+              <div
+                class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700"
+              >
+                <div
+                  class="bg-gray-600 h-2.5 rounded-full dark:bg-gray-300"
+                  style="width: 80%"
+                ></div>
+              </div>
+              <div
+                class="mb-1 text-base font-medium text-blue-700 dark:text-blue-500"
+              >
+                Vue.js 3
+              </div>
+              <div
+                class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700"
+              >
+                <div
+                  class="bg-blue-600 h-2.5 rounded-full"
+                  style="width: 60%"
+                ></div>
+              </div>
+              <div
+                class="mb-1 text-base font-medium text-red-700 dark:text-red-500"
+              >
+                Nuxt.js 3
+              </div>
+              <div
+                class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700"
+              >
+                <div
+                  class="bg-red-600 h-2.5 rounded-full dark:bg-red-500"
+                  style="width: 45%"
+                ></div>
+              </div>
+              <div
+                class="mb-1 text-base font-medium text-green-700 dark:text-green-500"
+              >
+                Tailwindcss 3 (Flowbite , Daisyui)
+              </div>
+              <div
+                class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700"
+              >
+                <div
+                  class="bg-green-600 h-2.5 rounded-full dark:bg-green-500"
+                  style="width: 60%"
+                ></div>
+              </div>
+              <div
+                class="mb-1 text-base font-medium text-yellow-700 dark:text-yellow-500"
+              >
+                Bootstrap 5 (BootstrapVue)
+              </div>
+              <div
+                class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700"
+              >
+                <div
+                  class="bg-yellow-400 h-2.5 rounded-full"
+                  style="width: 60%"
+                ></div>
+              </div>
+              <div
+                class="mb-1 text-base font-medium text-indigo-700 dark:text-indigo-500"
+              >
+                Node.js Expess.js (Basic)
+              </div>
+              <div
+                class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700"
+              >
+                <div
+                  class="bg-indigo-600 h-2.5 rounded-full dark:bg-indigo-500"
+                  style="width: 35%"
+                ></div>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
